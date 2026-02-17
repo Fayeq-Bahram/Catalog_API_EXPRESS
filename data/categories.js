53613a9063c96cf183c5e36d5c690b2c2e61ee8f
Fayeq Bahram <fayeq.bahram@gmail.com>
2026-02-17 10:08:59 +0500

    Add files via upload

---
+++ data/categories.js
@@ -0,0 +1,8 @@
+let categories = [
+    { id: 1, name: "Electronics" },
+    { id: 2, name: "Books" },
+    { id: 3, name: "Clothes" },
+    { id: 4, name: "Foods" },
+];
+
+module.exports = categories;
---
+++ data/products.js
@@ -0,0 +1,9 @@
+let products = [
+    { id: 1, name: "Iphone", categoryId: 1, price:  900},
+    { id: 2, name: "Laptop", categoryId: 1, price: 1200 },
+    { id: 3, name: "JavaScript Book", categoryId: 2, price: 26 },
+    { id: 4, name: "T-Shirt", categoryId: 3, price: 50 },
+    { id: 5, name: "Hum Burger", categoryId: 4, price: 3.5 },
+];
+
+module.exports = products;
